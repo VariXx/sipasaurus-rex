10d6eed535e4044ddaaaa10f12237aec6c7b66d3
VariXx <varixx@gmail.com>
2021-11-11 12:41:20 -0600

    added url to author in embed message, changed url timestamp

--- utils/streamingEmbed.js
+++ utils/streamingEmbed.js
@@ -7,15 +7,19 @@ async function streamingEmbed(twitchUsername, msgAuthor) {
         const twitchId = await twitchApi.getChannelID(twitchUsername, botSettings.twitchClientId, botSettings.twitchToken);
         const twitchInfo = await twitchApi.getTwitchUserInfo(twitchId, botSettings.twitchClientId, botSettings.twitchToken);
         const streamInfo = await twitchApi.getStreamInfo(twitchUsername, botSettings.twitchClientId, botSettings.twitchToken);
-        const thumbnailUrl = streamInfo.thumbnail_url.replace(`{width}x{height}.jpg`, `1920x1080.jpg#${Date.now()}`);
+        let urlTimestamp = Date.now();
+        const thumbnailUrl = streamInfo.thumbnail_url.replace(`{width}x{height}.jpg`, `1920x1080.jpg#${urlTimestamp}`);
         let streamTitle = Util.escapeMarkdown(streamInfo.title);
+        let channelUrl = `https://twitch.tv/${twitchInfo.display_name}`;
 
         const returnEmbed = new MessageEmbed()
         .setColor('#1EA8D7') // change this to use event color from channel info
         .setTitle(streamTitle)
-        .setURL(`https://twitch.tv/${twitchInfo.display_name}`) // change this to get from chnanel info
-        .setAuthor(`${msgAuthor} is live`)
+        .setURL(channelUrl) // change this to get from chnanel info
+        // .setAuthor(`${msgAuthor} is live`)
+        .setAuthor(`${msgAuthor} is live`, twitchInfo.profile_image_url, channelUrl)
         .setDescription(streamInfo.game_name)
+        // .setDescription(streamTitle)
         .setThumbnail(twitchInfo.profile_image_url)
         .setImage(thumbnailUrl) // change this to stream preview
         .setTimestamp()
