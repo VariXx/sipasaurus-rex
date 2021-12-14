2be178c6cf48f36381e0ffe0c0169e7c5c360080
Aaron <mail@aosterwyk.com>
2021-12-13 19:12:02 -0600

    added = to log level ifs

--- utils/log.js
+++ utils/log.js
@@ -4,9 +4,7 @@ const logChannel = require('../botSettings.json').logChannel;
 
 /*
 logLevel
-0: off
-1: error
-2: info
+
 */
 
 async function log(logType, channel, msg) {
@@ -14,13 +12,13 @@ async function log(logType, channel, msg) {
     let escMsg = Util.escapeMarkdown(msg);    
     if(logType.toLowerCase() == 'info') {
         logMsg = `[Info] ${msg}`;
-        if(logLevel > 2 && logChannel.length > 1) {
+        if(logLevel >= 2 && logChannel.length > 1) {
             await channel.send(escMsg);
         }  
     }  
     else if(logType.toLowerCase() == 'error') {
         logMsg = `[Error] ${msg}`;
-        if(logLevel > 1 && logChannel.length > 1) { 
+        if(logLevel >= 1 && logChannel.length > 1) { 
             await channel.send(escMsg);
         }
     }
