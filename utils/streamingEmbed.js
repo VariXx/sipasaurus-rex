6e0bf04be5cab79ce2af6c96d527b4d68a1d394f
Aaron <mail@aosterwyk.com>
2022-01-07 17:34:18 -0600

    removed old twitchApi stuff

--- utils/streamingEmbed.js
+++ utils/streamingEmbed.js
@@ -1,12 +1,9 @@
 const { MessageEmbed, Util } = require('discord.js');
 const twitchApi = require('./twitchApi');
-const botSettings = require('../botSettings.json');
 
 async function streamingEmbed(twitchUsername, msgAuthor) {
     try {
-        // const twitchInfo = await twitchApi.getTwitchUserInfo(twitchUsername, botSettings.twitchClientId, botSettings.twitchToken);
         const twitchInfo = await twitchApi.getTwitchUserInfo(twitchUsername);
-        // const streamInfo = await twitchApi.getStreamInfo(twitchUsername, botSettings.twitchClientId, botSettings.twitchToken);
         const streamInfo = await twitchApi.getStreamInfo(twitchUsername);
         let urlTimestamp = Date.now();
         const thumbnailUrl = streamInfo.thumbnail_url.replace(`{width}x{height}.jpg`, `1920x1080.jpg?${urlTimestamp}`);
@@ -16,7 +13,7 @@ async function streamingEmbed(twitchUsername, msgAuthor) {
         const returnEmbed = new MessageEmbed()
         .setColor('#1EA8D7') // change this to use event color from channel info
         .setTitle(streamTitle)
-        .setURL(channelUrl) // change this to get from chnanel info
+        .setURL(channelUrl) // change this to get from twitchInfo 
         .setAuthor(`${msgAuthor} is live`, twitchInfo.profile_image_url, channelUrl)
         .setDescription(streamInfo.game_name)
         .setThumbnail(twitchInfo.profile_image_url)
@@ -34,10 +31,8 @@ async function streamingEmbed(twitchUsername, msgAuthor) {
 
 async function offlineStreamingEmbed(twitchUsername, msgAuthor) {
     try {
-        // const twitchInfo = await twitchApi.getTwitchUserInfo(twitchUsername, botSettings.twitchClientId, botSettings.twitchToken);
         const twitchInfo = await twitchApi.getTwitchUserInfo(twitchUsername);
         let channelUrl = `https://twitch.tv/${twitchInfo.display_name}`;
-        // const twitchVideos = await twitchApi.getTwitchVideos(twitchUsername, botSettings.twitchClientId, botSettings.twitchToken);
         const twitchVideos = await twitchApi.getTwitchVideos(twitchUsername);
         const vodUrl = twitchVideos[0].url;
         const vodThumbnail = twitchVideos[0].thumbnail_url.replace('%{width}x%{height}', '1920x1080');
@@ -45,11 +40,9 @@ async function offlineStreamingEmbed(twitchUsername, msgAuthor) {
         // console.log(streamMarkers);
 
         const returnEmbed = new MessageEmbed() 
-        .setColor('#3d3d3f') // change this to use event color from channel info
-        // .setTitle(`${msgAuthor} was live`, twitchInfo.profile_image_url, channelUrl)
-        // .setTitle(`Click here to view VOD`, twitchInfo.profile_image_url, vodUrl)
+        .setColor('#3d3d3f') 
         .setTitle(twitchVideos[0].title, twitchInfo.profile_image_url, vodUrl)
-        .setURL(vodUrl) // change this to get from chnanel info
+        .setURL(vodUrl) // change this to get from twitchInfo 
         .setAuthor(`${msgAuthor} was live`, twitchInfo.profile_image_url, channelUrl)
         .setDescription(`${msgAuthor} is offline`)
         .setThumbnail(twitchInfo.profile_image_url)
