6607cc0d066a2b45bbbd58e30dd1090dc5ca303d
Aaron <mail@aosterwyk.com>
2023-03-04 01:43:32 -0600

    changed invite command message type

--- commands/invite.js
+++ commands/invite.js
@@ -6,7 +6,7 @@ module.exports = {
         .setDescription('link to add me to a server'),
     async execute(interaction) {
         const inviteUrl = `https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2147994688`; 
-        await interaction.reply(`Add me to your server: ${inviteUrl}`);   
+        await interaction.reply({ content: `Add me to your server: ${inviteUrl}`, ephemeral: true }); 
         return; 
     },
 };
