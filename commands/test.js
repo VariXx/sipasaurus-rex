5002443abfbdaf59d018cf46518d1181bce53386
Aaron <mail@aosterwyk.com>
2023-03-04 02:25:14 -0600

    added options to test command

--- commands/test.js
+++ commands/test.js
@@ -3,8 +3,14 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('test')
-        .setDescription('Sipa test'),
+        .setDescription('Sipa test')
+        .addStringOption(option =>
+            option.setName('option1')
+                .setDescription('option one')),
     async execute(interaction) {
-        await interaction.reply(`${interaction.user.username} ran the test command`);
+        const option1String = interaction.options.getString('option1');
+        await interaction.reply(`${interaction.user.username} ran the test command with option1: ${option1String}`);
     },
 };
+
+// https://discord.js.org/#/docs/builders/main/class/SlashCommandBuilder
