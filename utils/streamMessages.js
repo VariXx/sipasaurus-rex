4a164e366543d87240fea8c3d76d1af863637703
Aaron <mail@aosterwyk.com>
2023-05-21 21:40:22 -0500

    created streamMessages

---
+++ utils/streamMessages.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const util = require('util');
+const streamMessagesFilePath = require('../botSettings.json').streamMessagesFilePath;
+
+async function getStreamMessages() {
+    // read and return json
+    let returnStreamMessages = {};
+    const readFile = util.promisify(fs.readfile);
+    if(fs.existsSync(streamMessagesFilePath)) {
+        try {
+            const streamMessages = await readFile(streamMessagesFilePath);
+            returnStreamMessages = JSON.parse(streamMessages);
+            return returnStreamMessages;
+        }
+        catch(error) {
+            console.log(error);
+            return undefined;
+        }
+    }
+    else {
+        // file doesn't exist - should this exit or return blank?
+        return undefined; 
+    }   
+}
+
+async function writeStreamMessages(newMsg) {
+    try {
+        fs.writeFileSync(streamMessagesFilePath,JSON.stringify(newMsg));
+        return true;
+    }
+    catch(error) {
+        console.log(error);
+        return false;
+    }
+}
+
+module.exports.getStreamMessages = getStreamMessages;
+module.exports.writeStreamMessages = writeStreamMessages;
