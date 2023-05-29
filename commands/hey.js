4de6b1732ff0674ef35ec361d3b0c3ab566a4d3a
Aaron <mail@aosterwyk.com>
2023-05-29 11:08:06 -0500

    remove stream message from json after going offline, handle bot icon messages when no icon is set

--- commands/hey.js
+++ commands/hey.js
@@ -6,13 +6,15 @@ module.exports = {
         .setName('hey')
         .setDescription('say hello'),
     async execute(interaction) {
+        if(botSettings.botIcon !== undefined) { replyMsg = `${botSettings.botIcon}`; }
+        else { replyMsg = 'hi'; }        
         if(interaction.user.id == botSettings.botOwnerID) {
-            await interaction.reply(`${botSettings.botIcon}`);
+            await interaction.reply(replyMsg);
             return;
         }
         else {
             // still reply, but only reply to the user that send the command
-            await interaction.reply({ content: `${botSettings.botIcon}`, ephemeral: true });
+            await interaction.reply({ content: replyMsg, ephemeral: true });
             return;
         }
     },
