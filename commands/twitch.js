c7993a4f83086071ed1e2d7eb1c6c778b20b254e
Aaron <mail@aosterwyk.com>
2023-08-20 18:50:44 -0500

    added PermissionFlagsBits

--- commands/twitch.js
+++ commands/twitch.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const { setGuildSetting } = require('../utils/setGuildSetting');
 const botSettings = require('../botSettings.json');
 
