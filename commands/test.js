ef095629e3bce857dbea294c5746ea3d38680e5f
Aaron <mail@aosterwyk.com>
2023-08-20 16:42:31 -0500

    permissions test

--- commands/test.js
+++ commands/test.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const botSettings = require('../botSettings.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,8 +12,15 @@ module.exports = {
         // console.log(interaction);
         // console.log(interaction.guild.id);
         // console.log(interaction.channel);
-        const option1String = interaction.options.getString('option1');
-        await interaction.reply(`${interaction.user.username} ran the test command with option1: ${option1String}`);
+        // const option1String = interaction.options.getString('option1');
+        // await interaction.reply({ content: `${interaction.user.username} ran the test command with option1: ${option1String}`, ephemeral: true });
+
+        if(interaction.user.id == interaction.guild.ownerId || interaction.user.id == botSettings.botOwnerID) {
+            await interaction.reply('command was from guild or bot owner');
+        }
+        else {
+            await interaction.reply({content : 'Command restricted to guild or bot owner only.', ephemeral: true});
+        }
     },
 };
 
