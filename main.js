ec84195feed8a9177b16d0315c456dffec03668b
Aaron Osterwyk <mail@aosterwyk.com>
2023-12-20 18:15:10 -0600

    changed timers

--- main.js
+++ main.js
@@ -372,15 +372,14 @@ client.once('ready', async () => {
     // checkTwitchConnectionInterval = setInterval(twitchTokenHeartbeat,15000); // 15 seconds
     checkTwitchConnectionInterval = setInterval(twitchTokenHeartbeat,60*60000); // 1 hour 
     
-    refreshVStreamTokenTimer = setInterval(twitchTokenHeartbeat,45*60000); // 45 minutes
     // refreshVStreamTokenTimer = setInterval(checkVStreamToken,1*60000); // 60 seconds
+    refreshVStreamTokenTimer = setInterval(twitchTokenHeartbeat,45*60000); // 45 minutes    
     
-    cleanupStreamEmbedsTimer = setInterval(cleanupStreamEmbeds,1*40000); // 40 seconds
-    // cleanupStreamEmbedsTimer = setInterval(cleanupStreamEmbeds,1*60000); // 60 seconds
-    // cleanupStreamEmbedsTimer = setInterval(cleanupStreamEmbeds,10*60000); // 10 minutes (10*60000)
+    // cleanupStreamEmbedsTimer = setInterval(cleanupStreamEmbeds,1*40000); // 40 seconds
+    cleanupStreamEmbedsTimer = setInterval(cleanupStreamEmbeds,10*60000); // 10 minutes (10*60000)
     
-    checkStreamsTimer = setInterval(checkStreams,1*30000); // 30 seconds (1*10000)
-    // checkStreamsTimer = setInterval(checkStreams,10*60000); // 10 minutes (10*60000) 
+    // checkStreamsTimer = setInterval(checkStreams,1*30000); // 30 seconds (1*10000)
+    checkStreamsTimer = setInterval(checkStreams,10*60000); // 10 minutes (10*60000) 
     
 });
 
